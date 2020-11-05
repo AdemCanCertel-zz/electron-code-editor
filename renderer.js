155a9b928a9c476a74d8d5ecf4a709f313e6e7c6
AdemCan <69676558+AdemCan0BEY@users.noreply.github.com>
2020-11-05 21:10:46 +0300

    Updated

--- renderer.js
+++ renderer.js
@@ -1,12 +1,12 @@
 import loader from 'monaco-loader';
 import { remote } from 'electron';
-import FileManager from './filemanager'; // Dosya
+import FileManager from './filemanager'; // File
 
 loader().then((monaco) => {
   const editor = monaco.editor.create(document.getElementById('container'), {
-    language: 'python',  // Her dil için değiştirilir JavaScript ise javascript kodları kullanılır
-    language: 'javascript',  // Birden çok dil için altına ekleyin  ve filemanager.js gidip  this.editor.setModel(this.monaco.editor.createModel(data, 'javascript'));  alt alta dil ekleyin.
-    theme: 'vs-dark', // Kod Editörü tema rengi (Visual Code Dark Theme) olarak ayarlandı
+    language: 'python',  // It is changed for each language. If JavaScript, javascript codes are used.
+    language: 'javascript',  // For multiple languages, add it below and go to filemanager.js and go to this.editor.setModel (this.monaco.editor.createModel (data, 'javascript')); add language to the bottom.
+    theme: 'vs-dark', // Code Editor theme color set to (Visual Code Dark Theme)
     automaticLayout: true,
   });
 
